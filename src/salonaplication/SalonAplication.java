563a90e5378161b4659360e9294ca00165841010
Asepimam <17200931@bsi.ac.id>
2022-10-01 11:13:31 +0700

    second commit

---
+++ src/salonaplication/SalonAplication.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package salonaplication;
+
+/**
+ *
+ * @author aim
+ */
+public class SalonAplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TipeA obj = new TipeA();
+        obj.show();
+   }
+//    public String count(){
+//    }
+}
